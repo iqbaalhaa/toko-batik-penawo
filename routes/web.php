f42957f3448588125af33b2deda1159bb6e32731
Iqbaalhaa <iqbalhaanafi@gmail.com>
2026-04-19 16:53:42 +0700

    feat: implement product detail page and update product grid with links

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('home.index'))->name('home');
 Route::get('/produk', fn () => view('home.produk'))->name('produk');
+
+Route::get('/produk/{slug}', function (string $slug) {
+    $product = collect(config('products', []))->firstWhere('slug', $slug);
+    abort_unless($product, 404);
+
+    return view('home.produk-detail', ['product' => $product]);
+})->name('produk.detail');
+
 Route::get('/tentang', fn () => view('home.tentang'))->name('tentang');
 Route::get('/kontak', fn () => view('home.kontak'))->name('kontak');
